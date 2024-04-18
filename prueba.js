4b648b3ed686b94611896f6f72df1dd9f0f0f0d8
cristhiandona <cristhiandona@gmail.com>
2024-04-18 17:49:14 -0500

    Cambios

---
+++ prueba.js
@@ -0,0 +1,10 @@
+/*
+let lado = 40;
+let area = 40*40;
+let perimetro = 40*4;
+console.log("El area es "+ area);
+console.log("El perímetro es "+perimetro);*/
+
+/*let nombre = prompt("escribe tu nombre");
+let saludo = "Hola "+nombre;
+console.log(saludo);*/
